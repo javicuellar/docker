dfeba9eaa16415901c4ed9fee82d642520ed7fb5
javicuellar <javicu25@gmail.com>
2025-03-22 19:51:10 +0100

    Act. appweb_flask_SQLite (primera app_flask)

--- Python/lanzador.sh
+++ Python/lanzador.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-#  Ejecución script
-./appweb_flask_upload_file/ejecutar.sh
+#  Ejecución script, seleccionar la aplicación a ejecutar
+echo "---------------------------------------------------------"
+# ./appweb_flask_upload_file/ejecutar.sh
+
+./appweb_flask_SQLite/ejecutar.sh
